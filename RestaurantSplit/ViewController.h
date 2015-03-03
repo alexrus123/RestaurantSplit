d17000814d52aba07df6c416a90a7da227cea53d
Aliaksei Lyskovich <alex-alex@Aliakseis-MacBook-Pro.local>
2015-03-02 17:39:32 -0800

    Initial Commit

---
+++ RestaurantSplit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RestaurantSplit
+//
+//  Created by Aliaksei Lyskovich on 3/2/15.
+//  Copyright (c) 2015 Aliaksei Lyskovich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RestaurantSplit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RestaurantSplit
+//
+//  Created by Aliaksei Lyskovich on 3/2/15.
+//  Copyright (c) 2015 Aliaksei Lyskovich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
